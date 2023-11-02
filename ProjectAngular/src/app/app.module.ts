62f607e55e7a02c4ef21c5dc118fd30b68099f26
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-11-02 14:09:03 +0600

    second commit

--- ProjectAngular/src/app/app.module.ts
+++ ProjectAngular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogComponent,
+    GalleryComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule,
