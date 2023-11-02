d51ec3a88be00db4f6ad6af78b4e5e365b2cfc2b
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-11-02 16:41:10 +0600

    3rd commit

--- ProjectAngular/src/app/app-routing.module.ts
+++ ProjectAngular/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RoomComponent } from './room/room.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'room', component: RoomComponent},
+  {path:'gallery', component: GalleryComponent},
+  {path:'blog', component: BlogComponent},
+  {path:'contact', component: ContactComponent},
 ];
 
 @NgModule({
